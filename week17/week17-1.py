4e3341de1efc9defe7a6a90a775c08204bb930b6
PaiIan722 <ian2120812@gmail.com>
2024-01-02 11:55:53 +0800

    week17

---
+++ week17/week17-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        nums.sort() #先把數字排好 
+        ans = [[ nums[0] ]] #把最前面、最小的數字，放在兩方括號裡
+        repeat = 0 #目前的數字 nums[0] 沒有重覆
+        N = len(nums) #有幾個數字
+        for i in range(1,N): #想比較 nums[i] vs. nums[i-1]
+            if nums[i] == nums[i-1]: #這裡要處理，重覆，相同，就repeat += 1
+                repeat += 1
+                if len(ans)<=repeat: #目前的層數不夠多
+                    ans.append( [] ) #增加一層樓
+            else: #沒有重覆
+                    repeat = 0
+            ans[repeat].append( nums[i] )
+        return ans
